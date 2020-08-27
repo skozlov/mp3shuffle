6fcc73c14fac4e703218e1e4f3606baf214b4e7e
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2020-08-28 00:12:15 +0700

    mp3 shuffle

---
+++ src/com/github/skozlov/mp3shuffle/Main.java
@@ -0,0 +1,30 @@
+package com.github.skozlov.mp3shuffle;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    private static final String FILE_NAME_SUFFIX = ".mp3";
+
+    public static void main(String[] args) throws IOException {
+        var inDir = new File(args[0]);
+        var outDir = new File(args[1]);
+        //noinspection ResultOfMethodCallIgnored
+        outDir.mkdirs();
+        //noinspection ConstantConditions
+        var sourceFiles = Arrays.asList(inDir.listFiles((file, name) -> name.endsWith(FILE_NAME_SUFFIX)));
+        Collections.shuffle(sourceFiles);
+        var maxIndex = sourceFiles.size() - 1;
+        var indexLength = Integer.toString(maxIndex).length();
+        for (int i = 0; i <= maxIndex; i++) {
+            var sourceFile = sourceFiles.get(i);
+            var targetFileName = String.format("%0" + indexLength + "d", i) + FILE_NAME_SUFFIX;
+            var targetFile = new File(outDir, targetFileName);
+            System.out.println(String.format("%s -> %s", sourceFile.getAbsolutePath(), targetFile.getAbsolutePath()));
+            Files.copy(sourceFile.toPath(), targetFile.toPath());
+        }
+    }
+}
